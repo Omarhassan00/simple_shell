13c46c00f474b5f0a1026267ef4a2678fe266bed
Omarhassan00 <135049722+Omarhassan00@users.noreply.github.com>
2023-09-20 23:27:38 +0300

    Create shell.h

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+
+#endif
