ffd0d812cc0d0d35c897939afb9d4d601c9f51c6
omar hassan <omarhassan00123@gmail.com>
2023-09-20 21:31:21 +0000

    c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+/**
+* main - shell fanction order work
+* @n: number of alements
+* @b: elememnt
+* Return: © Always (success).
+*/
+
+int main(int n, char **b)
+
+{
+	 char *c = NLL;
+
+	 char **order = NULL;
+
+	 int store;
+
+	 (void) n;
+
+	 hile(1)
+
+	 c = read_line();
+
+	if
+		 (c == NULL) /* that is the ctr + D */
+
+	return (store);
+
+	order = tokenizer(c);
+
+	store = _execute(order, b);
+}
