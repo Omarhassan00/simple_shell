1b1ce036a9eb1f8ddf2ac6747d63fd404a580eac
Omarhassan00 <135049722+Omarhassan00@users.noreply.github.com>
2023-09-23 08:53:22 +0300

    Update string.c

--- string.c
+++ string.c
@@ -1,22 +1,15 @@
 #include "shell.h"
 
-/**
-* _strcat - concatenates two strings
-*
-* @dest: Destination string
-*
-* @src: Origin string
-* Return: Always 0
-*/
-
 char *_strcat(char *dest, char *src)
 {
 int i, j = 0;
 
-for (i = 0; dest[i] != '\0'; i++)
+for
+  (i = 0; dest[i] != '\0'; i++)
 {
 }
-for (j = 0; src[j] != '\0'; j++)
+for
+  (j = 0; src[j] != '\0'; j++)
 {
 dest[i] = src[j];
 i++;
@@ -24,16 +17,6 @@ i++;
 dest[i] = '\0';
 return (dest);
 }
-
-/**
-* _strcmp - Compares two strings
-*
-* @s1: a string
-* @s2: Another string
-*
-* Return: multiple returns
-*
-*/
 
 int _strcmp(char *s1, char *s2)
 {
@@ -46,35 +29,21 @@ val = s1[i] - s2[i];
 return (val);
 }
 
-/**
-* _strlen - gets the size of a string
-* @s: the string to measure
-* Return: the length of the string
-*/
-
 int _strlen(char *s)
 {
 int i;
-
-for (i = 0; s[i] != 0; i++)
-;
+for 
+  (i = 0; s[i] != 0; i++);
 
 return (i);
 }
-
-/**
-*_strncmp -  function that compares two strings.
-*@s1: string one
-*@s2: string two
-*@n: number of characters
-* Return: difference
-*/
 
 size_t _strncmp(char *s1, char *s2, size_t n)
 {
 size_t i, j;
 
-for (j = 0; s1[j] != '\0' && j < n; j++)
+for
+  (j = 0; s1[j] != '\0' && j < n; j++)
 {
 i = s1[j] - s2[j];
 
@@ -86,23 +55,15 @@ return (i);
 return (0);
 }
 
-/**
-* _strcpy - copies the string pointed to by src into dest
-* @dest: destination of the copy
-* @src: source of the copy
-*
-* Return: char pointer to dest
-*/
-
 char *_strcpy(char *dest, char *src)
 {
 int i = 0;
 
-for (i = 0; src[i] != '\0'; i++)
+for 
+  (i = 0; src[i] != '\0'; i++)
 {
 dest[i] = src[i];
 }
 dest[i + 1] = 0;
 return (dest);
 }
-
