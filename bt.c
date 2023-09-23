fe5ac7d3ddea1fce97e87696fd7092abadb98619
Omarhassan00 <135049722+Omarhassan00@users.noreply.github.com>
2023-09-24 02:05:02 +0300

    Update bt.c

--- bt.c
+++ bt.c
@@ -1,20 +1,20 @@
 #include "shell.h"
 
 /**
- * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: exits with a given exit status
- *         (0) if info.argv[0] != "exit"
+ * _myexit - exits from shell
+ * @info: make any information for the data type
+ * Return: it will exit for immediatele when type exit
  */
 int _myexit(info_t *info)
 {
 	int exitcheck;
 
-	if (info->argv[1])  /* If there is an exit arguement */
+	if
+		(info->argv[1])  /* If there is an exit arguement */
 	{
 		exitcheck = _erratoi(info->argv[1]);
-		if (exitcheck == -1)
+		if
+			(exitcheck == -1)
 		{
 			info->status = 2;
 			print_error(info, "Illegal number: ");
@@ -30,10 +30,9 @@ int _myexit(info_t *info)
 }
 
 /**
- * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * _mycd - changes the directory
+ * @info: it is info for any integer
+ *  Return: make 0 for success
  */
 int _mycd(info_t *info)
 {
@@ -43,18 +42,22 @@ int _mycd(info_t *info)
 	s = getcwd(buffer, 1024);
 	if (!s)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
-	if (!info->argv[1])
+	if
+		(!info->argv[1])
 	{
 		dir = _getenv(info, "HOME=");
-		if (!dir)
+		if
+			(!dir)
 			chdir_ret = /* TODO: what should this be? */
 				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
 		else
 			chdir_ret = chdir(dir);
 	}
-	else if (_strcmp(info->argv[1], "-") == 0)
+	else if
+		(_strcmp(info->argv[1], "-") == 0)
 	{
-		if (!_getenv(info, "OLDPWD="))
+		if
+			(!_getenv(info, "OLDPWD="))
 		{
 			_puts(s);
 			_putchar('\n');
@@ -66,7 +69,8 @@ int _mycd(info_t *info)
 	}
 	else
 		chdir_ret = chdir(info->argv[1]);
-	if (chdir_ret == -1)
+	if
+		(chdir_ret == -1)
 	{
 		print_error(info, "can't cd to ");
 		_eputs(info->argv[1]), _eputchar('\n');
@@ -80,10 +84,9 @@ int _mycd(info_t *info)
 }
 
 /**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * _myhelp - changes directory
+ * @info: it's give information for any tybe
+ *  Return: it's 0 for success
  */
 int _myhelp(info_t *info)
 {
