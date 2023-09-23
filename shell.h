cf535b915eea9de7b31c3724d1892c2f95ddb6cd
Omarhassan00 <135049722+Omarhassan00@users.noreply.github.com>
2023-09-23 08:51:32 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -2,23 +2,23 @@
 #define SHELL_H
 
 #include <stdio.h>
-#include <stdlib.h>
+
 #include <string.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
 #include <errno.h>
-
+#include <unistd.h>
 extern char **environ;
 
 int sh_exit(char **args);
 
 int _strcmp(char *s1, char *s2);
-
-size_t _strncmp(char *s1, char *s2, size_t n);
-
 int _strlen(char *s);
+size_t _strncmp(char *s1, char *s2, size_t n);
+int _values_path(char **arg, char **env);
+
 
 char *_strcpy(char *dest, char *src);
 
@@ -26,18 +26,18 @@ char *_strcat(char *dest, char *src);
 
 int _putchar(char c);
 
-char *_get_path(char **env);
+int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c);
 
-int _values_path(char **arg, char **env);
+char **tokenize(char *lineptr);
 
 char *_getline_command(void);
 
 void _getenv(char **env);
+char *_get_path(char **env);
 
-char **tokenize(char *lineptr);
 
 void _exit_command(char **args, char *lineptr, int _exit);
 
-int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c);
 
-#endif /* SHELL_H */
+
+#endif
